d6bd31c13dfd37b03aa58cbad70cb78d6d58cf41
a0m3 <almosawi0309@gmail.com>
2026-07-16 12:16:59 +0300

    Restaurant model done

--- models/Meal.js
+++ models/Meal.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const mealSchema = new mongoose.Schema({
 
-})
+}, { timestamps: true })
 
 const Meal = mongoose.model("Meal", mealSchema)
 module.exports = Meal
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose')
 
 const restaurantSchema = new mongoose.Schema({
-
-})
+    name: {
+        type: String,
+        required: true
+    },
+    logo: {
+        type: String
+    }
+}, { timestamps: true })
 
 const Restaurant = mongoose.model("Restaurant", restaurantSchema)
 module.exports = Restauratnt
