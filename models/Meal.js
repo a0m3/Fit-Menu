c581c5e2068ad21caccd36c6f6f5d467110b63c5
a0m3 <almosawi0309@gmail.com>
2026-07-16 12:17:32 +0300

    Meal model done

--- models/Meal.js
+++ models/Meal.js
@@ -1,7 +1,34 @@
 const mongoose = require('mongoose')
 
 const mealSchema = new mongoose.Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String
+    },
+    calories: {
+        type: Number,
+        required: true
+    },
+    protein: {
+        type: Number,
+    },
+    carbs: {
+        type: Number,
+    },
+    fat: {
+        type: Number,
+    },
+    price: {
+        type: Number
+    },
+    restaurant: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Restaurant',
+        required: true
+    }
 }, { timestamps: true })
 
 const Meal = mongoose.model("Meal", mealSchema)
